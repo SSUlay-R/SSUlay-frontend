64d91724ff575b8c8aa0fa24fdce00bc3f3ca06b
joseunghyo <seunghyo7742@naver.com>
2023-05-28 19:32:48 +0900

    Feat: 관리자페이지

--- ssulay-front/src/pages/AdminPage.jsx
+++ ssulay-front/src/pages/AdminPage.jsx
@@ -1,11 +1,13 @@
-import React, {useMemo, useEffect, useState, useRef} from 'react';
+import React, {useMemo,  useState} from 'react';
 import './AdminPage.css';
 import { collection, getDocs, } from "firebase/firestore";
 import { db } from '../config/firebase';
 import Table from '../components/Table';
+import Alert from 'react-bootstrap/Alert';
 
 export default function AdminPage() {
 
+  
   const columns= useMemo(
     ()=>[
       {
@@ -31,90 +33,39 @@ export default function AdminPage() {
       
     ],[]
   );
-  useEffect(() => {
-    const fetchData = async () => {
-      const usersCollection = collection(db, 'users');
-      const userSnapshot = await getDocs(usersCollection);
-      const userList = userSnapshot.docs.map(doc => {
-        const data = doc.data();
-        return {
-          userName: data.userName,
-          uid: data.uid,
-        };
-      });
-      console.log(userList); // Add this line to log your users data
-    };
 
-    fetchData();
-  }, []);
+// 교환학생과 내국인 학생 데이터를 분류해야 함. 
+const [isMatchingStarted, setIsMatchingStarted] = useState(false);
+const [stateMessage, setStateMessage] = useState('');
+const handleStartBtn=()=>{
+  setIsMatchingStarted(true);
+  setStateMessage('* 매칭이 시작되었습니다.')
+  //게일섀플리알고리즘 함수 돌리는거. 
+}
 
-  //여기에 matchedList data 불러와주세요
-  
-  const [searchPageOpen, setSearchPageOpen] = useState(true);
-  const [matchingStart, setMatchingStart] = useState(false);
-  const [resultPageOpen, setResultPageOpen] = useState(false);
-
-  const searchPageBtnRef = useRef(null);
-  const matchingStartBtnRef = useRef(null);
-  const resultPageOpenBtnRef = useRef(null);
-
-  //버튼 순서대로 상태 열리도록
-  useEffect(() => {
-    if (searchPageBtnRef.current && matchingStartBtnRef.current && resultPageOpenBtnRef.current) {
-      if (searchPageOpen) {
-        searchPageBtnRef.current.disabled = true;
-        matchingStartBtnRef.current.disabled = false;
-        resultPageOpenBtnRef.current.disabled = false;
-      } else if (matchingStart) {
-        searchPageBtnRef.current.disabled = true;
-        matchingStartBtnRef.current.disabled = true;
-        resultPageOpenBtnRef.current.disabled = false;
-      } else if (resultPageOpen) {
-        searchPageBtnRef.current.disabled = false;
-        matchingStartBtnRef.current.disabled = false;
-        resultPageOpenBtnRef.current.disabled = true;
-      }
-    }
-  }, [searchPageOpen, matchingStart, resultPageOpen]);
-
-
-  const handleSearchPageBtn = () => {
-    setSearchPageOpen(true);
-    setMatchingStart(false);
-    setResultPageOpen(false);
-    //유저들 isSubmittedForm 확인후  상태 메시지 보내기. 
-    //버디검색페이지 오픈되어있음을 데베에 전달
-  };
-
-  const handleMatchingStartBtn = () => {
-    setSearchPageOpen(false);
-    setMatchingStart(true);
-    setResultPageOpen(false);
-  };
-
-  const handleResultPageOpenBtn = () => {
-    setSearchPageOpen(false);
-    setMatchingStart(false);
-    setResultPageOpen(true);
-  };
+const handlePageOpenBtn=()=>{
+  setStateMessage('* 학생들이 결과 페이지를 확인할 수 있습니다.')
+}
   return (
     <>
       <div className="admin-page-contianier">
         <h1 className="admin-page-title">매칭 관리</h1>
         <hr />
         <div className="process-container">
-          <button ref={searchPageBtnRef} className="process-btn" onClick={handleSearchPageBtn}>
-            버디 검색 <br /> 페이지 오픈
-          </button>
-          <img className="arrow-img" alt="오른쪽 화살표" src="/assets/arrow.png"></img>
-          <button ref={matchingStartBtnRef} className="process-btn" onClick={handleMatchingStartBtn}>
+          <button className="process-btn" onClick={handleStartBtn} disabled={isMatchingStarted}>
             매칭 시작
           </button>
-          <img className="arrow-img" alt="오른쪽 화살표" src="/assets/arrow.png"></img>
-          <button ref={resultPageOpenBtnRef} className="process-btn" onClick={handleResultPageOpenBtn}>
+          <img className="arrow-img" alt="오른쪽 화살표" src="/assets/arrow.png" ></img>
+          <button className="process-btn" onClick={handlePageOpenBtn} disabled={!isMatchingStarted} >
             매칭 확인<br />페이지 오픈
           </button>
         </div>
+        <div className="state-message">
+        {stateMessage &&(
+          <Alert variant='secondary'>{stateMessage}</Alert>
+        )}
+        </div>
+
         <div className="matching-result-container">
           <h1 className="admin-page-title">매칭 결과</h1>
           <hr />
