8160ad66dfe7d3d293829bd024089cf294ec76d1
joseunghyo <seunghyo7742@naver.com>
2023-05-30 18:40:34 +0900

    Feat: interestTag db 구분

--- ssulay-front/src/pages/CompletePage.jsx
+++ ssulay-front/src/pages/CompletePage.jsx
@@ -1,29 +1,32 @@
 import React,{useEffect} from 'react';
 import "./CompletePage.css";
 import { Link } from 'react-router-dom';
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, getDocs,query, where, collectionGroup } from 'firebase/firestore';
 import { db } from '../config/firebase';
 
 
 export default function CompletePage() {
 
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const usersCollection = collection(db, 'fakeUser');
-      const userSnapshot = await getDocs(usersCollection);
-      const userList = userSnapshot.docs.map((doc) => {
-        const data = doc.data();
-        return {
-          interestTag: data.interestTag,
-        };
-      });
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const usersCollection = collection(db, 'fakeUser','test');
+  //     const docRef= collection(db,"interestTags");
+  //     const interestTags = query(usersCollection, where('interestTag','==',docRef));
+  //     const userSnapshot = await getDocs(usersCollection);
+  //     const userList = userSnapshot.docs.map((doc) => {
+  //       const data = doc.data();
+  //       return {
+  //         test:data.test,
+  //         interestTag: interestTags,
+  //       };
+  //     });
 
-      console.log(userList);
-    };
+  //     console.log(userList);
+  //   };
 
-    fetchData();
-  }, []);
+  //   fetchData();
+  // }, []);
 
   return (
     <>
