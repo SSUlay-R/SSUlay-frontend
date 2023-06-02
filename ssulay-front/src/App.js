880471c6eca0084a2367c61a6a85bf333d3b8940
joseunghyo <seunghyo7742@naver.com>
2023-06-02 23:18:46 +0900

    머지 시켜주라

--- ssulay-front/src/App.js
+++ ssulay-front/src/App.js
@@ -17,7 +17,7 @@ import { AuthContext } from './context/AuthContext';
 import BuddyProgramPage from './pages/BuddyProgramPage';
 import AdminPage from './pages/AdminPage';
 import DummyDataPage from './pages/DummyDataPage';
-import GaleShapelyAlgorithm from "./function/GaleShapelyAlgorithm";
+import GaleShapelyAlgorithm from "./Function/GaleShapelyAlgorithm";
 
 function App() {
   const { currentUser } = useContext(AuthContext);
