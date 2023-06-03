2dd9b59c246a317e95189da986070d9d30a604cf
joseunghyo <seunghyo7742@naver.com>
2023-06-03 13:42:14 +0900

    Feat: 유사도 반복문 테스트 완료

--- ssulay-front/src/pages/CompletePage.jsx
+++ ssulay-front/src/pages/CompletePage.jsx
@@ -1,36 +1,64 @@
-import React,{useEffect, useState} from 'react';
-import "./CompletePage.css";
+import React, { useEffect, useState } from 'react';
+import './CompletePage.css';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 export default function CompletePage() {
-  const[similarity, setsimilarity] =useState(null);
-  const keyword1= 'apple';
-  const keyword2= 'orange';
+  const [similarityArr, setSimilarityArr] = useState([]);
 
-  useEffect(()=>{
-    axios.get('/similarity',{
-      params:{ keyword1 : keyword1,
-        keyword2: keyword2}
-    })
-    .then(response=>setsimilarity(response.data.similarity))
-    .catch(error=>console.error(error));
-  })
+  useEffect(() => {
+    const fetchData = async () => {
+      const preferedInterests = ['sports', 'apple', 'dance','television','pizza']; // 더미 데이터로 채워주세요
+      const tagInterests = ['baseball', 'study', 'cafe']; // 더미 데이터로 채워주세요
+
+      const arr = [];
+      for (const preferedInterest of preferedInterests) {
+        let avgSimilarity = 0;
+        const count = preferedInterests.length * tagInterests.length;
+        const weight = 1 / Math.sqrt(count);
+
+        for (const tagInterest of tagInterests) {
+          let similarity = null;
+          try {
+            const response = await axios.get('/similarity', {
+              params: { keyword1: preferedInterest, keyword2: tagInterest },
+            });
+            similarity = response.data.similarity;
+            console.log(`Similarity between ${preferedInterest} and ${tagInterest}: ${similarity}`);
+          } catch (error) {
+            console.error(error);
+          }
+
+          avgSimilarity += similarity * weight;
+          console.log(`Intermediate avgSimilarity: ${avgSimilarity}`);
+        }
+
+        avgSimilarity /= count;
+        console.log(`Final avgSimilarity: ${avgSimilarity}`);
+        arr.push({ similarity: avgSimilarity });
+      }
+
+      setSimilarityArr(arr);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
       <div className="container">
         <div className="explain-container">
-        Thank you for submitting the form. You’ll be notified when we’re ready for you to be matched with buddy.
+          Thank you for submitting the form. You’ll be notified when we’re ready for you to be matched with a buddy.
         </div>
-        <Link to="/"><button className="submit-btn">Confirm
-        </button></Link>
-        {similarity !==null ? (
-          <p> Similarity: {similarity}</p>
-        ):(
+        <Link to="/">
+          <button className="submit-btn">Confirm</button>
+        </Link>
+        {similarityArr.length > 0 ? (
+          <p>Similarity: {similarityArr.map((item) => item.similarity).join(', ')}</p>
+        ) : (
           <p>Loading</p>
         )}
       </div>
-      
     </>
-  )
+  );
 }
